eb3b7111353572d4e0833516a9f0f5c6babda063
sharoon001 <104424854+sharoon001@users.noreply.github.com>
2022-05-05 10:37:35 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "keypair1"
+  default     =  "keypair2"
 }
 
 variable "instance_type" {
